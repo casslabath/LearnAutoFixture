6714521d40ca8c15b767b1c29b2ff0806799862e
casslabath <casslabath@yahoo.com>
2022-03-28 14:12:27 -0500

    add first test with autofixture

---
+++ LearnAutoFixture.Tests/LearnAutoFixture.Tests/NumberDemos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace LearnAutoFixture.Tests
+{
+    public class NumberDemos
+    {
+        [Fact]
+        public void Ints()
+        {
+            // arange
+            var sut = new IntCalculator(); //sut is System Under Test
+
+            // act
+            sut.Subtract(1);
+
+            // assert
+            Assert.True(sut.Value < 0);
+        }
+    }
+}
--- LearnAutoFixture/IntCalculator.cs
+++ LearnAutoFixture/IntCalculator.cs
@@ -1,4 +1,4 @@
-﻿namespace DemoCode
+﻿namespace LearnAutoFixture
 {
     public class IntCalculator
     {
