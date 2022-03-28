e642bb806aa9957c2e24daabe13f5a6c1df37369
casslabath <casslabath@gmail.com>
2022-03-28 15:37:58 -0500

    creating email addresses

--- 02 - Creating Anon Test Data and Objects/DemoCode.Tests/DemoCode.Tests/GuidEnumsDemos.cs
+++ 02 - Creating Anon Test Data and Objects/DemoCode.Tests/DemoCode.Tests/GuidEnumsDemos.cs
@@ -13,6 +13,22 @@ namespace DemoCode.Tests
             var sut = new EmailMessage(fixture.Create<string>(),
                                        fixture.Create<string>(),
                                        fixture.Create<bool>());
+
+            sut.Id = fixture.Create<Guid>();
+        }
+
+        [Fact]
+        public void Enum()
+        {
+            var fixture = new Fixture();
+            var sut = new EmailMessage(fixture.Create<string>(),
+                                       fixture.Create<string>(),
+                                       fixture.Create<bool>());
+
+            sut.Id = fixture.Create<Guid>();
+
+            // Loops through each part of the enum 
+            sut.MessageType = fixture.Create<EmailMessageType>();
         }
     }
 }
