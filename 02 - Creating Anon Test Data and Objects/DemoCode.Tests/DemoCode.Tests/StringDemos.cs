824d0057e5d61f9e7f133e77aab42531e6241d44
casslabath <casslabath@yahoo.com>
2022-03-28 14:53:41 -0500

    creating anon strings

---
+++ 02 - Creating Anon Test Data and Objects/DemoCode.Tests/DemoCode.Tests/StringDemos.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using AutoFixture;
+
+namespace DemoCode.Tests
+{
+    public class StringDemos
+    {
+        [Fact]
+        public void BasicStirngs()
+        {
+            // arrange
+            var fixture = new Fixture();
+            var sut = new NameJoiner();
+
+            /*
+             * Learned through debug that the default string 
+             * created by fixture is a GUID
+             */
+            //var firstName = fixture.Create<string>();
+            //var lastName = fixture.Create<string>();
+
+            /*
+             * Giving a seeded string can make debugging much 
+             * easier
+             */
+            var firstName = fixture.Create("First_");
+            var lastName = fixture.Create("Last_");
+
+            // act
+            var result = sut.Join(firstName, lastName);
+
+            // assert
+            Assert.Equal(firstName + " " + lastName, result);
+        }
+
+        [Fact]
+        public void Chars()
+        {
+            var fixture = new Fixture();
+
+            /*
+             * debugged to look at default
+             */
+            var anonChar = fixture.Create<char>();
+        }
+    }
+}
